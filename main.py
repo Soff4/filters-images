8eb7c5b2fcf9889ee1c64c6d03c3789849b350b8
Soff4 <sofa00sofa@gmail.com>
2023-11-24 22:14:36 +0200

    update file for work

--- main.py
+++ main.py
@@ -40,11 +40,6 @@ vertical_sobel = np.array([
     [-2, 0, 2],
     [-1, 0, 1]])
 
-horizontal_sobel = np.array([
-    [-1, -2, -1],
-    [0, 0, 0],
-    [1, 2, 1]])
-
 border = np.array([
     [-1, -1, 0], 
     [-1, 8, -1], 
@@ -54,11 +49,6 @@ invert = np.array([
     [-1, -1, -1],
     [-1, 8, -1],
     [-1, -1, -1]])
-
-kernel_filer = np.array([
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]], dtype=np.uint8)
 
 def handler_convolve(img, filter):
     img_height, img_width = img.shape
@@ -76,79 +66,6 @@ def handler_convolve(img, filter):
 
     return output_image
 
-def handler_erosion(img, filter):
-    img_rows, img_cols = img.shape
-    filter_rows, filter_cols = filter.shape
-    
-    eroded_image = np.zeros((img_rows, img_cols), dtype=np.uint8)
-
-    for r in range(1, img_rows - 1):
-        for c in range(1, img_cols - 1):
-            min_val = 255
-
-            for filter_r in range(filter_rows):
-                for filter_c in range(filter_cols):
-                    val = img[r - 1 + filter_r][c - 1 + filter_c] - filter[filter_r][filter_c]
-
-                    if val < min_val:
-                        min_val = val
-
-            eroded_image[r][c] = min_val
-
-    return eroded_image
-
-def handler_dilation(img, filter):
-    img_rows, img_cols = img.shape
-    filter_rows, filter_cols = filter.shape
-    
-    dilated_image = np.zeros((img_rows, img_cols), dtype=np.uint8)
-
-    for r in range(1, img_rows - 1):
-        for c in range(1, img_cols - 1):
-            max_val = 0
-
-            for filter_r in range(filter_rows):
-                for filter_c in range(filter_cols):
-                    val = img[r - 1 + filter_r][c - 1 + filter_c] + filter[filter_r][filter_c]
-
-                    if val > max_val:
-                        max_val = val
-
-            dilated_image[r][c] = max_val
-
-    return dilated_image
-
-def handler_opening(img, filter):
-    erosion_img = handler_erosion(img, filter)
-    opening_image = handler_dilation(erosion_img, filter)
-    return opening_image
-
-def handler_closing(img, filter):
-    dilation_img = handler_dilation(img, filter)
-    closing_image = handler_erosion(dilation_img, filter)
-    return closing_image
-
-def handler_edges(img):
-    img_rows, img_cols = img.shape
-
-    gradient_x = np.zeros((img_rows - 2, img_cols - 2))
-    gradient_y = np.zeros((img_rows - 2, img_cols - 2))
-    gradient_mg = np.zeros((img_rows - 2, img_cols - 2))
-
-    for r in range(img_rows - 2):
-        for c in range(img_cols - 2):
-            window = img[r:r+3, c:c+3]
-            gradient_x[r, c] = np.sum(window * horizontal_sobel)
-            gradient_y[r, c] = np.sum(window * vertical_sobel)
-            gradient_mg[r, c] = np.sqrt(gradient_x[r, c]**2 + gradient_y[r, c]**2)
-
-    return gradient_mg
-
-def handler_show_image(img, title):
-    plt.imshow(img, cmap='gray')
-    plt.title(title)
-    plt.show()
-
 identity_image = handler_convolve(image, identity)
 shifted_image = handler_convolve(image, shift)
 gaussian_image = handler_convolve(image, gaussian)
@@ -157,10 +74,28 @@ sharpness_image = handler_convolve(image, sharpness)
 vertical_sobel_image = handler_convolve(image, vertical_sobel)
 border_image = handler_convolve(image, border)
 invert_image = handler_convolve(image, invert)
-eroded_image = handler_erosion(image, kernel_filer)
-dilated_image = handler_dilation(image, kernel_filer)
-opening_image = handler_opening(image, kernel_filer)
-closing_image = handler_closing(image, kernel_filer)
-edges_of_image = handler_edges(image)
 
-handler_show_image(edges_of_image, 'Edges of Image')
+fig, axs = plt.subplots(3, 4)
+axs[0, 0].imshow(image, cmap='gray')
+axs[0, 0].set_title('Original')
+axs[0, 1].imshow(identity_image, cmap='gray')
+axs[0, 1].set_title('Identity Filter')
+axs[0, 2].imshow(shifted_image, cmap='gray')
+axs[0, 2].set_title('Shift Filter')
+axs[0, 3].imshow(gaussian_image, cmap='gray')
+axs[0, 3].set_title('Gaussian Filter')
+
+axs[1, 0].imshow(motion_blur_image, cmap='gray')
+axs[1, 0].set_title('Motion Blur Filter')
+axs[1, 1].imshow(sharpness_image, cmap='gray')
+axs[1, 1].set_title('Sharpness Filter')
+axs[1, 2].imshow(vertical_sobel_image, cmap='gray')
+axs[1, 2].set_title('Vertical Sobel Filter')
+axs[1, 3].imshow(border_image, cmap='gray')
+axs[1, 3].set_title('Border Filter')
+
+axs[2, 0].imshow(invert_image, cmap='gray')
+axs[2, 0].set_title('Invert Filter')
+
+plt.tight_layout()
+plt.show()
